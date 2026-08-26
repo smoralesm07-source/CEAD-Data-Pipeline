8d5dda629d8c594190463912bfe0cb1eadfb7360
smoralesm07-source <smoralesm07@gmail.com>
2026-08-26 09:38:10 -0400

    cead: refresh official document catalog before enrichment

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from cead_pipeline.core import run_pipeline
+from cead_pipeline.documents import refresh_document_catalog
 from cead_pipeline.enrichment import build_enrichment_outputs
 
 if __name__ == "__main__":
     core = run_pipeline()
+    documents = refresh_document_catalog()
     enrichment = build_enrichment_outputs()
-    print({"core": core, "enrichment": enrichment})
+    print({"core": core, "documents": documents, "enrichment": enrichment})
